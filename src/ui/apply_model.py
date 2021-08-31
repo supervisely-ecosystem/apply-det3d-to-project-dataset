4ca21c2bef6cfc48c241acf58f1eec0ec7f3a610
kashkarov <kashkarov@dedicorp.ru>
2021-08-31 12:28:54 +0300

    Loading button fix

--- src/ui/apply_model.py
+++ src/ui/apply_model.py
@@ -7,9 +7,6 @@ def init(data, state):
     state["disabled5"] = True
     data["done5"] = False
 
-    data["progress"] = None
-    data["progressCurrent"] = None
-    data["progressTotal"] = None
     data["resProjectId"] = None
     data["resProjectName"] = None
     data["started"] = False
@@ -18,14 +15,6 @@ def init(data, state):
 @g.my_app.callback("apply_model")
 @sly.timeit
 def apply_model(api: sly.Api, task_id, context, state, app_logger):
-
-    def _update_progress(progress):
-        fields = [
-            {"field": "data.progress", "payload": int(progress.current * 100 / progress.total)},
-            {"field": "data.progressCurrent", "payload": progress.current},
-            {"field": "data.progressTotal", "payload": progress.total},
-        ]
-        api.task.set_fields(task_id, fields)
 
     # Setup new project
     project_names = [x.name for x in api.project.get_list(g.workspace_id)]
@@ -38,29 +27,34 @@ def apply_model(api: sly.Api, task_id, context, state, app_logger):
 
 
     pointclouds = api.pointcloud.get_list(new_dataset.id)
-    progress = sly.Progress("Inference", len(pointclouds), need_info_log=True)
 
     params = {"pointcloud_ids": [p.id for p in pointclouds],
-              "threshold": state["confThres"] }
+              "threshold": state["confThres"]}
 
     new_annotations = g.api.task.send_request(state['sessionId'], "inference_pointcloud_ids", data=params)
     new_annotations = new_annotations["results"]
 
     # update meta
-    filtered_meta = g.model_meta.clone()
+    if state["addMode"] == "merge":
+        res_project_meta = g.project_meta.merge(g.model_meta)
+    elif state["addMode"] == "replace":
+        res_project_meta = g.model_meta
+        api.project.update_meta(new_project.id, sly.ProjectMeta)
+    else:
+        raise ValueError("Wrong add mode")
 
-    res_project_meta = g.project_meta.merge(filtered_meta) if state["addMode"] == "merge" else filtered_meta
     api.project.update_meta(new_project.id, res_project_meta.to_json())
 
     for i, pc in enumerate(pointclouds):
-        progress.iter_done()
-        api.pointcloud.annotation.append(pc.id, sly.PointcloudAnnotation.from_json(new_annotations[i], g.project_meta))
+        api.pointcloud.annotation.append(pc.id, sly.PointcloudAnnotation.from_json(new_annotations[i], res_project_meta))
+
 
 
     fields = [
         {"field": "data.resProjectId", "payload": new_project.id},
         {"field": "data.resProjectName", "payload": new_project.name},
-        #{"field": "data.resProjectPreviewUrl", "payload": api.image.preview_url(new_project.reference_image_url, 100, 100)},
+        {"field": "data.started", "payload": False},
+        {"field": "data.done5", "payload": True}
     ]
     api.task.set_fields(task_id, fields)
     api.task.set_output_project(task_id, new_project.id, new_project.name)
